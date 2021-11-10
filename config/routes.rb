cc6a6e6f93f577ff953f1db73fde99698c9c340e
shibu1225 <keisuke.shibusawa@gmail.com>
2021-11-10 11:12:52 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get root :to => "homes#top"
+  get 'books' => "books#index"
+  get 'books/new' => "books#new"
+  get 'books/show' => "books#show"
+  get 'books/edit' => "books#edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
